6fddd4f43a86b7d1d20f0681ca0fe0c96577ee9c
werwolfy <raphael.nigl@outlook.de>
2024-12-23 20:30:23 +0100

    [INIT] Final Setup

---
+++ src/main/java/github/werwolfy/MagicWorldBelow/MagicWorld.java
@@ -0,0 +1,7 @@
+package github.werwolfy.MagicWorldBelow;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(MagicWorld.MODID)
+public class MagicWorld {
+    public static final String MODID = "magicworlbelow";
+}
